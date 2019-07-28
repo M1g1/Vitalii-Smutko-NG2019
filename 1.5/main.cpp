58a08be0fbbd5355418d0bf35de28a6306b878d8
M1g1 <43749241+M1g1@users.noreply.github.com>
2019-07-28 14:09:19 +0300

    Completed task 1.5

---
+++ 1.5/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Vvedite chislo: ";
+    cin >> num;
+     for (int i=0;i<=num;i++)
+        cout << i << " ";
+    return 0;
+}

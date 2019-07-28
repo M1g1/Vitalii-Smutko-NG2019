21c921a8dcac23a4c39d53e431b06ee6a4b077a1
M1g1 <43749241+M1g1@users.noreply.github.com>
2019-07-28 14:06:33 +0300

    Completed task 1.4

---
+++ 1.4/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int stars;
+    cout << "Skolko vivesti zvyozdochek?";
+    cin >> stars;
+     for(int i=0;i<stars;i++)
+        cout << '*';
+    return 0;
+}

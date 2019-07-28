5d509b25608b59c28f35951c1876014dabf706f4
M1g1 <43749241+M1g1@users.noreply.github.com>
2019-07-28 12:56:46 +0300

    Completed task 1.1

---
+++ 1.1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float first_num=0,second_num=0;
+    cout << "Enter the first number: ";
+    cin >> first_num;
+    cout << "Enter the second number: ";
+    cin >> second_num;
+    cout << "Result: " << first_num+second_num;
+    return 0;
+}

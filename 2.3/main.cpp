68d4459c3b23a782c8e858262cb1fae7e682c2b2
M1g1 <43749241+M1g1@users.noreply.github.com>
2019-08-06 14:34:08 +0300

    Done without "break"

--- 2.3/main.cpp
+++ 2.3/main.cpp
@@ -5,22 +5,21 @@ using namespace std;
 int main()
 {
     int num_of_stars[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-        number_of_entered_numbers=0, maxElement, i, space_and_star;
-     for (i=0; i<20 ; i++)
+        number_of_entered_numbers=0, maxElement, i=0, space_and_star;
+     do
      {
          cout << "Vvedite chislo: ";
          cin >> num_of_stars[i];
-         if (num_of_stars[i]==0)
-            break;
          number_of_entered_numbers++;
-     }
+         i++;
+     }while (num_of_stars[i-1]!=0 && i<20);
      maxElement=num_of_stars[0];
      for (i=1 ; i<number_of_entered_numbers ; i++) //to find the maximum number entered
         if (num_of_stars[i]>maxElement)
           maxElement=num_of_stars[i];
      for (i=0; i<number_of_entered_numbers ; i++)
      {
-      for (space_and_star=0; space_and_star<(maxElement-num_of_stars[i])/2 ; space_and_star++) //cycle for space
+      for (space_and_star=0; space_and_star<(maxElement-num_of_stars[i])/2.0 ; space_and_star++) //cycle for space
          cout << ' ';
        for (space_and_star=0; space_and_star<num_of_stars[i] ; space_and_star++) //cycle for stars
           cout << '*';

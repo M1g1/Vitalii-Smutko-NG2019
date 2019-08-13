23660c124c189bdc8207f2bb3216c9edd106f455
M1g1 <43749241+M1g1@users.noreply.github.com>
2019-08-13 19:03:39 +0300

    Сode simplifications

--- 3.1/main.cpp
+++ 3.1/main.cpp
@@ -8,17 +8,14 @@ int main()
     int i=0, count=0;
     cout << "Enter your string: ";
     cin.getline(str, 100);
+    if (str[0]>='A' && str[0]<='Z' || str[0]>='a' && str[0]<='z')
+        count=1;
      while(str[i]!='\0')
      {
-         if (str[i]<'A' || str[i]>'Z' && str[i]<'a' || str[i]>'z')
-         {
-             count++;
-             if (str[i+1]<'A' || str[i+1]>'Z' && str[i+1]<'a' || str[i+1]>'z')
-                count--;
-         }
+         if ((str[i]<'A' || str[i]>'Z' && str[i]<'a' || str[i]>'z') &&
+            (str[i+1]>='A' && str[i+1]<='Z' || str[i+1]>='a' && str[i+1]<='z'))
+              count++;
          i++;
      }
-     if (str[0]>='A' && str[0]<='Z' || str[0]>='a' && str[0]<='z')
-        count++;
      cout << "Word count: " << count;
 }

f3a8e5c3a1b90e5b2f6c09bf4a38a64c336a70ad
M1g1 <43749241+M1g1@users.noreply.github.com>
2019-08-15 20:00:06 +0300

    Reduced number of passes on string

--- 3.4/main.cpp
+++ 3.4/main.cpp
@@ -4,31 +4,26 @@ using namespace std;
 
 int main()
 {
-    char str[256], alphabet[26], temp_ch;
-    int count[26], i=0, j=0, temp=0;
+    char str[256], alphabet[27], temp_ch;
+    int count[26]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    i=0, j=0, temp=0;
     cout << "Enter your string: ";
     cin.getline(str, 256);
-    for(j=0; j<26; j++)
-    {
-     i=0;
-     count[j]=0;
       while (str[i]!=0)
       {
-       if (str[i]>='a' && str[i]<='z' || str[i]>='A' && str[i]<='Z' )
-        {
-         if (str[i]==65+j || str[i]==97+j)
-         {
-         count[j]++;
-         alphabet[j]=65+j;
-         }
-        }
-        i++;
+       for(j=0;j<26;j++)
+          if (str[i]=='a'+j || str[i]=='A'+j)
+          {
+            count[j]++;
+            if(count[j]==1)
+                alphabet[j]='A'+j;
+          }
+         i++;
       }
-    }
     //Sorting
     for(j=0; j<25; j++)
         for(i=0; i<25-j; i++)
-            if (count[i]<count[i+1])
+         if (count[i]<count[i+1])
             {
                 temp=count[i];
                 temp_ch=alphabet[i];
@@ -37,9 +32,10 @@ int main()
                 count[i+1]=temp;
                 alphabet[i+1]=temp_ch;
             }
+
     for (j=0; j<26; j++)
         if(count[j]!=0)
-          cout << alphabet[j] << " - " << count[j] << endl;
+           cout << alphabet[j] << " - " << count[j] << endl;
 
     return 0;
 }

094bff57d47bcd60c385f61a15f45270016fc051
M1g1 <43749241+M1g1@users.noreply.github.com>
2019-08-16 11:32:31 +0300

    Сode simplifications

--- 3.1/main.cpp
+++ 3.1/main.cpp
@@ -8,12 +8,10 @@ int main()
     int i=0, count=0;
     cout << "Enter your string: ";
     cin.getline(str, 100);
-    if (str[0]>='A' && str[0]<='Z' || str[0]>='a' && str[0]<='z')
-        count=1;
      while(str[i]!='\0')
      {
-         if ((str[i]<'A' || str[i]>'Z' && str[i]<'a' || str[i]>'z') &&
-            (str[i+1]>='A' && str[i+1]<='Z' || str[i+1]>='a' && str[i+1]<='z'))
+         if ((str[i]>='A' && str[i]<='Z' || str[i]>='a' && str[i]<='z') &&
+             (str[i+1]<'A' || str[i+1]>'Z' && str[i+1]<'a' || str[i+1]>'z'))
               count++;
          i++;
      }

7ec030c9a0a640741888d13bdda5fd713bcaf408
M1g1 <43749241+M1g1@users.noreply.github.com>
2019-08-16 12:00:07 +0300

    Сode simplifications

--- 2.3/main.cpp
+++ 2.3/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int num_of_stars[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-        number_of_entered_numbers=0, maxElement, i=0, space_and_star;
+        maxElement=0, i=0, space_and_star;
      do
      {
          cout << "Vvedite chislo: ";
@@ -16,20 +16,20 @@ int main()
               cout << "Vvedite chislo: ";
               cin >> num_of_stars[i];
           }
-         number_of_entered_numbers++;
+          if (num_of_stars[i]>maxElement)
+              maxElement=num_of_stars[i];
          i++;
      }while (num_of_stars[i-1]!=0 && i<20);
-     maxElement=num_of_stars[0];
-     for (i=1 ; i<number_of_entered_numbers ; i++) //to find the maximum number entered
-        if (num_of_stars[i]>maxElement)
-          maxElement=num_of_stars[i];
-     for (i=0; i<number_of_entered_numbers ; i++)
+     for (i=0; i<20 ; i++)
      {
-      for (space_and_star=0; space_and_star<(maxElement-num_of_stars[i])/2 ; space_and_star++) //cycle for space
-         cout << ' ';
-       for (space_and_star=0; space_and_star<num_of_stars[i] ; space_and_star++) //cycle for stars
-          cout << '*';
+       if (num_of_stars[i]>0)
+       {
+          for (space_and_star=0; space_and_star<(maxElement-num_of_stars[i])/2 ; space_and_star++) //cycle for space
+              cout << ' ';
+          for (space_and_star=0; space_and_star<num_of_stars[i] ; space_and_star++) //cycle for stars
+              cout << '*';
        cout << endl;
+       }
      }
     return 0;
 }
--- 2.4/main.cpp
+++ 2.4/main.cpp
@@ -4,16 +4,14 @@ using namespace std;
 
 int main()
 {
-    int num_of_stars[5]={0,0,0,0,0}, maxElement, i;
+    int num_of_stars[5]={0,0,0,0,0}, maxElement=0, i;
      for (i=0 ; i<5 ; i++)
      {
          cout << "Zvezdochek v " << i+1 << "-om stolbike: ";
          cin >> num_of_stars[i];
-     }
-     maxElement=num_of_stars[0];
-     for (i=1 ; i<5 ; i++) //to find the maximum number entered
-        if (num_of_stars[i]>maxElement)
+         if (num_of_stars[i]>maxElement)
            maxElement=num_of_stars[i];
+     }
      for (i=0 ; i<maxElement ; i++)
      {
         for (int j=0 ; j<5 ; j++)
